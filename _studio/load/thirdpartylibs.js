7fd088eff19c68a8d40ee9b16a8594debe388213
Henning Klokkeråsen <henning.klokkeraasen@gmail.com>
2015-04-18 13:05:18 +0200

    Added tick marks for input range
    only works for slider-vertical

--- _studio/load/thirdpartylibs.js
+++ _studio/load/thirdpartylibs.js
@@ -3,11 +3,25 @@ define([
     '/_thirdparty/handlebars-v1.3.0.js'
     ],
 	function() {
+
+		// a Handlebars helper that provides equality if condition
 		Handlebars.registerHelper('ifCond', function(v1, v2, options) {
 		  if(v1 === v2) {
 		    return options.fn(this);
 		  }
 		  return options.inverse(this);
+		});
+
+		// a Handlebars helper to get the middle of two values
+		Handlebars.registerHelper('middleValue', function(v1, v2) {
+		  var middleValue = (v1+v2)/2;
+		  return middleValue;
+		});
+
+		// a Handlebars helper to get the hundreth of the sum of two values, multiplied by the multiplier
+		Handlebars.registerHelper('percentage', function(v1, v2, multiplier) {
+		  var result = (v1+v2)/100*multiplier;
+		  return result;
 		});
 	}
 );
