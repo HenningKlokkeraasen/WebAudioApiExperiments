e18f5109d54bf208eb97b6f1674116b5b1e615a5
Henning Klokkeråsen <henning.klokkeraasen@gmail.com>
2016-01-31 15:28:45 +0100

    Added basic FM, AM and Filter Cutoff Modulation patches

---
+++ _studio/racks/Modulation/vibrato.js
@@ -0,0 +1,51 @@
+define([
+	'/_studio/Modules/BasicWaa/Analyser/Analyser.js',
+	'/_studio/Modules/Composite/MasterSection/MasterSection.js',
+
+	'/_studio/Modules/BasicWaa/Oscillator/Oscillator.js',
+	'/_studio/Modules/BasicWaa/Filter/Filter.js',
+	'/_studio/Modules/BasicWaa/Gain/Gain.js',
+
+	'/_studio/Modules/CustomModulators/LFO/Lfo.js'
+	], function(Analyser, MasterSection, Oscillator, Filter, Gain, LFO) {
+		return {
+			title : 'Frequency Modulation / Tremolo',
+			description : 'This rack shows modulation of an oscillator with an LFO. '
+				+'The LFO modulates the oscillators frequency. This is known as FM. The effect is also known as a vibrato effect.'
+				
+				,
+			rackData : {
+				rows : [
+					{
+						modules : [
+							{ moduleMother: LFO, id: 'lfo1' },
+							{ moduleMother: Oscillator, id: 'osc5' },
+							{ moduleMother: Filter, id: 'filter5' },
+							{ moduleMother: Gain, id: 'gain5' }
+						],
+					},
+					{
+						modules: [
+							{ moduleMother: MasterSection, id: 'masterSection1' },
+							{ moduleMother: Analyser, id: 'analyser1' },
+							{ moduleMother: Analyser, id: 'analyser2' }
+						]
+					}
+				],
+				patches : [
+					// Main audio route
+					{ from : 'osc5', to : 'filter5', type : 'audio' },
+					{ from : 'filter5', to : 'gain5', type : 'audio' },
+					{ from : 'gain5', to : 'masterSection1', type : 'audio' },
+					
+					// Modulation
+					{ from: 'lfo1', to: 'osc5', type: 'control' },
+					
+					// visual
+					{ from: 'masterSection1', to: 'analyser1', type: 'audio' },
+					{ from: 'masterSection1', to: 'analyser2', type: 'audio' }
+				]
+			},
+		};
+	}
+);
---
+++ _studio/racks/Modulation/wah-wah.js
@@ -0,0 +1,51 @@
+define([
+	'/_studio/Modules/BasicWaa/Analyser/Analyser.js',
+	'/_studio/Modules/Composite/MasterSection/MasterSection.js',
+
+	'/_studio/Modules/BasicWaa/Oscillator/Oscillator.js',
+	'/_studio/Modules/BasicWaa/Filter/Filter.js',
+	'/_studio/Modules/BasicWaa/Gain/Gain.js',
+
+	'/_studio/Modules/CustomModulators/LFO/Lfo.js'
+	], function(Analyser, MasterSection, Oscillator, Filter, Gain, LFO) {
+		return {
+			title : 'Filter Cutoff Modulation / Wah-Wah / Filter Sweep / Growl',
+			description : 'This rack shows modulation of a filter with an LFO. '
+				+'The LFO modulates the filters cutoff frequency. '
+				+'At about 0.1Hz it gives a filter sweep, 1-2Hz gives wah-wah, 10-20Hz gives growl.'
+				+' (http://www.soundonsound.com/sos/feb00/articles/synthsecrets.htm)',
+			rackData : {
+				rows : [
+					{
+						modules : [
+							{ moduleMother: LFO, id: 'lfo1' },
+							{ moduleMother: Oscillator, id: 'osc5' },
+							{ moduleMother: Filter, id: 'filter5' },
+							{ moduleMother: Gain, id: 'gain5' }
+						],
+					},
+					{
+						modules: [
+							{ moduleMother: MasterSection, id: 'masterSection1' },
+							{ moduleMother: Analyser, id: 'analyser1' },
+							{ moduleMother: Analyser, id: 'analyser2' }
+						]
+					}
+				],
+				patches : [
+					// Main audio route
+					{ from : 'osc5', to : 'filter5', type : 'audio' },
+					{ from : 'filter5', to : 'gain5', type : 'audio' },
+					{ from : 'gain5', to : 'masterSection1', type : 'audio' },
+					
+					// Modulation
+					{ from: 'lfo1', to: 'filter5', type: 'control' },
+					
+					// visual
+					{ from: 'masterSection1', to: 'analyser1', type: 'audio' },
+					{ from: 'masterSection1', to: 'analyser2', type: 'audio' }
+				]
+			},
+		};
+	}
+);
